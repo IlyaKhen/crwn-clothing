2b9bf5a8c86f7a40d51df18af790cd9defd06fdb
ilya.khenkin <khenkhan451@gmail.com>
2021-12-27 11:19:00 +0200

    collection-preview

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  
-  <>
-    <h1>HATS PAGE</h1>
-  </>
-)
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </>
   );
